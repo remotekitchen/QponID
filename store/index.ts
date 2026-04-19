244e989a000400854d20ffb92a06ecde60f8c1e3
Merina Mou <merinamou7@gmail.com>
2026-04-19 11:22:09 +0600

    in home page i add restaurant deal information

--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '@/store/authSlice';
+import { apiSlice } from '@/store/apiSlice';
+import '@/store/grouponApi';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
